eb072e2c591b96f797cb092c3d1cdc20a9a8973a
polcaparross <polcaparrosescudero@gmail.com>
2024-04-22 23:11:58 +0200

    Done

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Whac-a-Mole</h1>
+  <div style="display: flex; flex-wrap: wrap">
+    <Mole></Mole>
+    <Mole></Mole>
+    <Mole></Mole>
+    <Mole></Mole>
+    <Mole></Mole>
+    <Mole></Mole>
+  </div>
+</template>
+
+<script>
+import Mole from './components/Mole.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Mole
+  }
+};
+</script>
---
+++ src/components/Mole.vue
@@ -0,0 +1,51 @@
+<script>
+import empty from '../empty.png'
+import mole from '../mole.png'
+import ouch from '../ouch.png'
+export default {
+  data() {
+    return {
+      name: 'Mole',
+      actual: empty,
+      alive: true,
+      eliminated: false
+    }
+  },
+  created() {
+    this.startInterval()
+  },
+  methods: {
+    startInterval() {
+      setInterval(() => {
+        if (!this.eliminated) {
+          if (this.actual == mole) {
+            this.actual = empty
+          } else {
+            this.actual = mole
+          }
+        }
+      }, Math.random() * 1000 + 500)
+    },
+    matar() {
+      if (this.actual == mole) {
+        this.actual = ouch
+        this.eliminated = true
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="moleHole" id="mole" @click="matar()">
+    <img :src=actual>
+  </div>
+</template>
+
+
+<style>
+.moleHole {
+  border: solid 1px black;
+  margin: 2px;
+}
+</style>
